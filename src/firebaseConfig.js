2fd7bfdc15f384aa2783fedef830eed6aaa62ba5
Charlize Briggs <charlizebriggs@gmail.com>
2024-10-29 14:57:23 -0700

    sign in working

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDN9scknb4kvRnyDN6MLnYjaHtre-M4Jxg",
   authDomain: "char-blog-88aa1.firebaseapp.com",
@@ -13,9 +9,8 @@ const firebaseConfig = {
   storageBucket: "char-blog-88aa1.appspot.com",
   messagingSenderId: "1044734896896",
   appId: "1:1044734896896:web:9da01aec366f5c51daa1e5"
-};
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
